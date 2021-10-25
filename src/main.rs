4f4b59782ba79c310f06d856aa2f53675cb57544
scottejames <scottejames@gmail.com>
2021-10-25 17:43:10 +0100

    Initial push

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
 use rocket::response::Redirect;
+mod utils;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -10,48 +13,19 @@ fn index() -> &'static str {
 
 #[get("/search?<cmd>")]
 fn search(cmd: String) -> Redirect {
-    println!("You typed in: {}", cmd);
-        let command = get_command_from_query_string(&cmd);
+    println!("You typed in {}", cmd);
 
-    	let redirect_url = match command {
-            "tw" => String::from("https://twitter.com"),
-          	_ => String::from("https://google.com")
-        };
+    let command = utils::get_command_from_query_string(&cmd);
+
+    let redirect_url = match command {
+        "gh" => utils::github::construct_github_url(&cmd),
+        "tw" => utils::twitter::construct_twitter_url(&cmd),
+        _ => utils::google::construct_google_search_url(&cmd),
+    };
 
     Redirect::to(redirect_url)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index,search]).launch();
-}
-
-fn get_command_from_query_string(query_string: &str) -> &str {
-        if query_string.contains(' ') {
-                // We need to this to know where to slice the string
-                let index_of_space = query_string.find(' ').unwrap_or(0);
-                return &query_string[..index_of_space];
-            }
-       // Otherwise, return the query string as is
-        &query_string
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_command_from_query_string_no_whitespace() {
-        // Test with command only
-        let actual = get_command_from_query_string("tw");
-        let expected = "tw";
-        assert_eq!(actual, expected);
-    }
-    #[test]
-    fn test_get_command_from_query_string_with_whitespace() {
-        let actual = get_command_from_query_string
-            ("tw @fbOpenSource");
-        let expected = "tw";
-        assert_eq!(actual, expected);
-    }
+    rocket::ignite().mount("/", routes![index, search]).launch();
 }
